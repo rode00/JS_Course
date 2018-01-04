b8864b925022167c9576114c0a51a0498e89aebf
rode00 <rodneyelworthy@Rodneys-MacBook.local>
2018-01-04 17:17:53 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// //
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with a number
+
+
+// //Multi name var
+
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sarah'; // Underscore
+// var Firstname = 'Tom'; //Pascal case
+// var 
+
+// const
+
+// const name = 'John';
+// console.log(name);
+
+//name = 'Sarah';
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+person.name = 'Sara';
+person.age = 32
+//console.log(person);
+
+const numbers = [1,2,3,4];
+numbers.push(6);
+
+
+console.log(numbers);
